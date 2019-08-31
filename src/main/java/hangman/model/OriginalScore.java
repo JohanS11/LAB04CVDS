c3a61fd1c5021989537acbce73d5bed51ff62f35
unknown <2146195@labinfo.is.escuelaing.edu.co>
2019-08-31 08:13:15 -0500

    Especificaciones métodos

--- src/main/java/hangman/model/OriginalScore.java
+++ src/main/java/hangman/model/OriginalScore.java
@@ -8,6 +8,8 @@ public class OriginalScore implements GameScore {
 	 * @param int incorrectCount determina cuantas veces ha fallado el usuario
 	 * @pre el puntaje inicia con 100 puntos
 	 * @pos el puntaje debe ser mayor o igual 0 
+	 * @pos No se bonifican las letras correctas
+	 * @pos Se penaliza con 10 puntos con cada letra incorrecta.
 	 * @return int es el puntaje total obtenido por el usuario con los valores especificados
 	 */
 	public int calculateScore(int correctCount, int incorrectCount) {
--- src/main/java/hangman/model/PowerScore.java
+++ src/main/java/hangman/model/PowerScore.java
@@ -8,6 +8,8 @@ public class PowerScore implements GameScore {
 	 * @param int incorrectCount determina cuantas veces ha fallado el usuario
 	 * @pre el puntaje inicia con 0 puntos
 	 * @pos el puntaje debe ser menor o igual 500 y mayor o igual 0 
+	 * @pos La $i-ésima$ letra correcta se bonifica con $5^i$
+	 * @pos Se penaliza con 8 puntos cada letra incorrecta. 
 	 * @return int es el puntaje total obtenido por el usuario con los valores especificados
 	 */
 	public int calculateScore(int correctCount, int incorrectCount) {
